68368805374a54f18e63746e185f1eefe88e75cc
Pipe Carbonell <carbonellandres11@gmail.com>
2022-06-03 10:07:19 -0500

    Crud Terminada

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,8 +16,10 @@ const Form = ({newLocation}) => {
         <div className="container">
             <form onSubmit={onSubmit}>
                 <div className="input-group mb-3 mx-auto">
-                    <input type="text" className="form-control" placeholder="Ciudad" onChange={(e) =>setCity(e.target.value)}/>
-                    <button className="btn btn-primary input-group-text" type="submit">Buscar</button>
+                    <input type="text" className="form-control" 
+                    placeholder="Ciudad" onChange={(e) =>setCity(e.target.value)}/>
+                    <button className="btn btn-primary input-group-text" type="submit">
+                        Buscar</button>
                 </div>
             </form>
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
 const NavBar = () => {
     return(
 
-        <nav className="navbar bg-dark text-light mb-5">
+        <nav className="navbar text-light mb-5">
             <div className="container-fluid">
-                <h3 className="mx-auto">Predicción Meteorológica</h3>
+                <h3 className="mx-auto">Weather App Api</h3>
             </div>
         </nav>
 
